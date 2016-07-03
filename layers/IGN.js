5f8cc8d78ba3bf159eeb93b6d46f597b08e5ebef
Dominique Cavailhez <protec@cavailhez.fr>
2016-07-03 22:41:05 +0200

    Updates (optim https)

--- layers/IGN.js
+++ layers/IGN.js
@@ -6,7 +6,7 @@
  * Cartes françaises
  * Projections Lambert et couches IGN
  * Clé de développement (localhost) sur: http://api.ign.fr
- * Clé de production sur: http://pro.ign.fr/api-web => Service en ligne => S'ABONNER
+ * Clé de production sur: http://pro.ign.fr/api-web => Service en ligne => Pour un site internet grand public => S'ABONNER
  * Doc sur http://api.ign.fr/jsp/site/Portal.jsp?page_id=6&document_id=80&dossier_id=53
  *
  * Layers (dépend de l'abonnement lié à votre clé)
@@ -14,27 +14,30 @@
 	new L.TileLayer.IGN({k:'CLE_IGN', l:'ORTHOIMAGERY.ORTHOPHOTOS'}) : Photos
 	new L.TileLayer.IGN({k:'CLE_IGN', l:'GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD'}) : TOP 25
 	new L.TileLayer.IGN({k:'CLE_IGN', l:'GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE'}) : Nouvelle présentation
-	new L.TileLayer.IGN({k:'CLE_IGN', l:'CADASTRALPARCELS.PARCELS'}) : Cadastre
+	new L.TileLayer.IGN({k:'CLE_IGN', l:'CADASTRALPARCELS.PARCELS', f: 'png'}) : Cadastre
  */
 
 L.TileLayer.IGN = L.TileLayer.extend({
 	options: {
+		p: window.location.href.match(/[a-z]*/i)[0], // Use the same protocol than the referer.
 		l: 'GEOGRAPHICALGRIDSYSTEMS.MAPS',
+		f: 'jpeg',
 		attribution: '&copy; <a href="http://www.ign.fr/">IGN</a>'
 	},
 	initialize: function(options) {
 		L.TileLayer.prototype.initialize.call(this,
-			"http://gpp3-wxs.ign.fr/{k}/wmts" +
-				"?LAYER={l}"+
-				"&FORMAT=image/jpeg"+
-				"&SERVICE=WMTS"+
-				"&VERSION=1.0.0"+
-				"&REQUEST=GetTile"+
-				"&STYLE=normal"+
-				"&TILEMATRIXSET=PM"+
-				"&TILEMATRIX={z}"+
-				"&TILECOL={x}"+
-				"&TILEROW={y}",
+			'{p}://wxs.ign.fr/{k}/geoportail/wmts' +
+				'?LAYER={l}'+
+				'&EXCEPTIONS=text/xml'+
+				'&FORMAT=image/{f}'+
+				'&SERVICE=WMTS'+
+				'&VERSION=1.0.0'+
+				'&REQUEST=GetTile'+
+				'&STYLE=normal'+
+				'&TILEMATRIXSET=PM'+
+				'&TILEMATRIX={z}'+
+				'&TILECOL={x}'+
+				'&TILEROW={y}',
 			options
 		);
 	}
